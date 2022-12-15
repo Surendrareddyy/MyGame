6f30b67339e5be60d07550673d2ca8dcc16aa834
Surendrareddyy <38355080+Surendrareddyy@users.noreply.github.com>
2022-12-15 14:25:25 +0200

    Create Main.py
    Proof of Concept

---
+++ Main.py
@@ -0,0 +1,8 @@
+
+
+Dictionary1 = {1 : "Surendra", 2 : "Surendra", 3 : "Surendra", 4 : "Surendra" }
+Dictionary2 = {5 : "Meera", 6 : "Meera", 7 : "Meera", 8 : "Meera"}
+
+Dictionary = Dictionary1 + Dictionary2
+
+print(Dictionary)
